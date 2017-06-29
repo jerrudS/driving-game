c82be4f7f3002069dc8efc893de0035322151966
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-06-29 16:37:08 -0700

    Make a simple car show up on the web document. #1

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 
-const carUl = document.createElement('ul')
-const wheel1 = document.createElement('li')
-const wheel2 = document.createElement('li')
-const wheel3 = document.createElement('li')
-const wheel4 = document.createElement('li')
+function createCar() {
+  const carUl = document.createElement('ul')
+  const wheel1 = document.createElement('li')
+  const wheel2 = document.createElement('li')
+  const wheel3 = document.createElement('li')
+  const wheel4 = document.createElement('li')
+  const smiley = document.createElement('li')
+  const eye1 = document.createElement('li')
+  const eye2 = document.createElement('li')
 
-carUl.appendChild(wheel1)
-carUl.appendChild(wheel2)
-carUl.appendChild(wheel3)
-carUl.appendChild(wheel4)
+  wheel1.setAttribute('class', 'one')
+  wheel2.setAttribute('class', 'two')
+  wheel3.setAttribute('class', 'three')
+  wheel4.setAttribute('class', 'four')
+  smiley.setAttribute('class', 'smile')
+  eye1.setAttribute('class', 'eye1')
+  eye2.setAttribute('class', 'eye2')
 
-const carDiv = document.querySelector('.car')
+  carUl.appendChild(wheel1)
+  carUl.appendChild(wheel2)
+  carUl.appendChild(wheel3)
+  carUl.appendChild(wheel4)
+  carUl.appendChild(smiley)
+  carUl.appendChild(eye1)
+  carUl.appendChild(eye2)
 
-carDiv.appendChild(carUl)
+  const carDiv = document.querySelector('.car')
+
+  carDiv.appendChild(carUl)
+}
+createCar()
